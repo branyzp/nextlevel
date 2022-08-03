91b7375b35bf97cc0e2494f3d5d90889f91e5ee1
Brandon <branyzp@gmail.com>
2022-08-03 16:07:21 +0800

    updated skills

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import ProjectItem from './ProjectItem';
 
 function Projects() {
 	return (
-		<div className="p-2">
-			<p className=" text-xl py-4 uppercase tracking-widest text-[#5651e5]">
-				Projects
-			</p>
-			<h2 className="py-4 text-gray-600">What I've built</h2>
-			<div className="py-2 uppercase">
-				<p>Calorition</p>
+		<div className="w-full">
+			<div className="max-w-[1240px] mx-auto px-2 py-16">
+				<p className=" text-xl py-4 uppercase tracking-widest text-[#5651e5]">
+					Projects
+				</p>
+				<h2 className="py-4 text-gray-600">What I've built</h2>
+				<div className="grid md:grid-cols-2 gap-8">
+					<ProjectItem />
+				</div>
 			</div>
 		</div>
 	);
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -11,108 +11,85 @@ function Skills() {
 				<div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn.icon-icons.com/icons2/2415/PNG/512/html_original_wordmark_logo_icon_146478.png"
-								alt="html"
-							/>
+							<img src="https://i.imgur.com/H4RmTcC.png" alt="html" />
 							<h3>HTML</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn.icon-icons.com/icons2/2415/PNG/512/css_original_wordmark_logo_icon_146576.png"
-								alt="css"
-							/>
+							<img src="https://i.imgur.com/TFy4JuG.png" alt="css" />
 							<h3>CSS</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png"
-								alt="javascript"
-							/>
+							<img src="https://i.imgur.com/dOEwTGJ.png" alt="javascript" />
 							<h3>JavaScript</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_python_icon_130221.png"
-								alt="python"
-							/>
+							<img src="https://i.imgur.com/D4UkydR.png" alt="python" />
 							<h3>Python</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png"
-								alt="react"
-							/>
+							<img src="https://i.imgur.com/lfdzAIO.png" alt="react" />
 							<h3>React</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn-icons-png.flaticon.com/512/5968/5968381.png"
-								alt="typescript"
-							/>
+							<img src="https://i.imgur.com/LAfAdBG.png" alt="typescript" />
 							<h3>TypeScript</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn.icon-icons.com/icons2/2415/PNG/512/nodejs_original_wordmark_logo_icon_146412.png"
-								alt="node"
-							/>
+							<img src="https://i.imgur.com/4PwdUpP.png" alt="node" />
 							<h3>Node</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_wordmark_logo_icon_146425.png"
-								alt="mongodb"
-							/>
+							<img src="https://i.imgur.com/q2fj2AI.png" alt="mongodb" />
 							<h3>mongoDB</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-								alt="github"
-							/>
+							<img src="https://i.imgur.com/HyL3whS.png" alt="github" />
 							<h3>GitHub</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img
-								src="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_tailwind_icon_130128.png"
-								alt="tailwind"
-							/>
+							<img src="https://i.imgur.com/XTk0vGA.png" alt="tailwind" />
 							<h3>Tailwind</h3>
 						</div>
 					</div>
 
 					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
 						<div className="grid grid-cols-2 gap-4 justify-center items-center">
-							<img src="https://v4.mui.com/static/logo.png" alt="materialui" />
+							<img src="https://i.imgur.com/FmLnvyJ.png" alt="materialui" />
 							<h3>Material UI</h3>
+						</div>
+					</div>
+
+					<div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+						<div className="grid grid-cols-2 gap-4 justify-center items-center">
+							<img src="https://i.imgur.com/0uNFHDC.png" alt="materialui" />
+							<h3>NextJS</h3>
 						</div>
 					</div>
 				</div>
